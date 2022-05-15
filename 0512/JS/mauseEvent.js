3331b4a2eaa0dbbd108a352439f25bc55c2e312c
ROKO02 <90667780+ROKO02@users.noreply.github.com>
2022-05-15 16:11:57 +0900

    Add files via upload

--- 0512/JS/mauseEvent.js
+++ 0512/JS/mauseEvent.js
@@ -17,21 +17,21 @@ function hoverOut(e) {
 }
 
 mauseOn[0].addEventListener("mouseover", hoverOn(0));
-mauseOn[0].addEventListener("mouseout", hoverOut(0));
 subMenuOn[0].addEventListener("mouseover", hoverOn(0));
+mauseOn[0].addEventListener("mouseout", hoverOut(0));
 subMenuOn[0].addEventListener("mouseout", hoverOut(0));
 
 mauseOn[1].addEventListener("mouseover", hoverOn(1));
-mauseOn[1].addEventListener("mouseout", hoverOut(1));
 subMenuOn[1].addEventListener("mouseover", hoverOn(1));
+mauseOn[1].addEventListener("mouseout", hoverOut(1));
 subMenuOn[1].addEventListener("mouseout", hoverOut(1));
 
 mauseOn[2].addEventListener("mouseover", hoverOn(2));
-mauseOn[2].addEventListener("mouseout", hoverOut(2));
 subMenuOn[2].addEventListener("mouseover", hoverOn(2));
+mauseOn[2].addEventListener("mouseout", hoverOut(2));
 subMenuOn[2].addEventListener("mouseout", hoverOut(2));
 
 mauseOn[3].addEventListener("mouseover", hoverOn(3));
-mauseOn[3].addEventListener("mouseout", hoverOut(3));
 subMenuOn[3].addEventListener("mouseover", hoverOn(3));
+mauseOn[3].addEventListener("mouseout", hoverOut(3));
 subMenuOn[3].addEventListener("mouseout", hoverOut(3));
